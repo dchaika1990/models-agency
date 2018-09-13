f83bd6840095a13da053e7b8404bc98aef665bca
Unknown <dchaika@inbox.ru>
2018-09-13 14:33:47 +0300

    desctop end

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,13 +3,19 @@ $(document).ready(function () {
 
     // MixitUp
 
-    mixitup($('#mixit'))
+    mixitup($('#mixit'));
 
     // magnificPopup
 
-    $('.mix').magnificPopup({
+    $('.section_tabs__body').magnificPopup({
         delegate: 'a',
-        type: 'image'
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
     });
 
 });
